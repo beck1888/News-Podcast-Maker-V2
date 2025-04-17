0af305ad83eb20c0b8b364a350d7453be5978803
Beck Orion <beckorion@gmail.com>
2025-04-17 07:47:31 -0700

    feat: implement success validation for scraped news articles

---
+++ apis/text_gen.py
@@ -0,0 +1,28 @@
+import json
+from openai import OpenAI
+
+def get_prompt(id: str) -> str:
+    with open('apis/prompts.json', 'r', encoding='utf-8') as f:
+        prompts: dict = json.load(f)
+    return prompts[id]
+
+def was_scraped_successfully(scraped_data: str, api_key: str) -> bool:
+    client: OpenAI = OpenAI(api_key=api_key)
+
+    model_response: str = client.chat.completions.create(
+        model='gpt-4.1-nano',
+        messages=[
+            {
+                'role': 'system',
+                'content': get_prompt('check_for_successful_scrape')
+            },
+            {
+                'role': 'user',
+                'content': f'Here is the extracted story: {scraped_data}'
+            }
+        ]
+    ).choices[0].message.content.lower()
+
+    if 'true' in model_response:
+        return True
+    return False # Fallback
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from apis.fetch_news import get_top_stories
 from apis.scrape import scrape_page
+from apis.text_gen import was_scraped_successfully
 from tools.environment_manager import get_environmental_variable
 from tools.terminal import spinner
 
@@ -14,16 +15,34 @@ def main() -> None:
         news: list[str] = get_top_stories(news_api_key)
 
     # Collect output
-    headlines: dict = {}
+    collected_news: dict = {}
     for index, story in enumerate(news, start=1):
         with spinner(f'Reading story {str(index)} of 5'):
             url = story[1]
-            headlines[index] = {
+            collected_news[index] = {
                 'headline': story[0],
                 'story': scrape_page(url)
             }
 
-    print(headlines)
+    # Build and validate list of stories
+    final_news: list[str] = []
+    for index in range(len(collected_news)):
+        index = index + 1 # Start at one because I did something goofy with the last code block to make it more clear in the terminal ui
+        with spinner(f'Validating scrape {str(index)} of 5'):
+            headline: str = collected_news[index]['headline']
+            story: str = collected_news[index]['story']
+            is_scrape_sucsessful: bool = was_scraped_successfully(story, openai_api_key)
+            if is_scrape_sucsessful:
+                final_news.append(f'Headline: {headline.upper()} ---- Article body: {story}')
+        print(f"Scrape sucsess? {str(is_scrape_sucsessful)}")
+    
+    print(f"Final amount of sucsessfully collected stories: {str(len(final_news))}")
+    print('\n'*10)
+    print(final_news)
+
+
+
+
 
 
 
